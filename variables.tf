700c40f8e1587d83c8de142370b396b6f1150932
Amir Tayabali <amir.tayabali@appvia.io>
2024-11-29 13:43:59 +0000

    add the type

--- variables.tf
+++ variables.tf
@@ -3,16 +3,19 @@
 
 variable "region" {
   description = "AWS region"
+  type        = string
   default     = "us-west-1"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
+  type        = string
   default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
+  type        = string
   default     = "Provisioned by Terraform"
 }
 
